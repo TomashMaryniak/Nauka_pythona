79f8732f671b8c861256354bd6c9b6e1fe36b402
tomashmaryniak <tomashmary@user.noreply.github.com>
2019-11-08 22:23:55 +0100

    hasla.py

--- hasla.py
+++ hasla.py
@@ -2,5 +2,8 @@ print("Progrma maskujcy haslo")
 print("Podaj haslo do zamaskowania")
 haslo = 'Maryniak123'
 dl_hasla = len(haslo)
-for x in xrange(dl_hasla):
-    print("Haslo: "+haslo[0] haslo[-1])
+zz = "*"
+print(zz)
+# for x in xrange(dl_hasla):
+#    print("Haslo: "+haslo[0], haslo[-1])
+print("Haslo: "+haslo[0],4*zz, haslo[-1])
